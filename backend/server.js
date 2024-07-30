48d1a83c9d9339132d26cf9d1dc7648cd904774d
rishita <rgrover_be21@thapar.edu>
2024-07-31 02:15:45 +0530

    space

--- backend/server.js
+++ backend/server.js
@@ -14,7 +14,6 @@ app.use(cors({ credentials: true, origin: ["http://localhost:3001", ""] }));
 app.use('/user', studentRouter); // Mount the router
 app.use('/user', coinRouter); // Mount the router
 
-
 const PORT=process.env.PORT || 3000
 app.listen(PORT,()=>{
     console.log(`port listening on port on port  ${process.env.PORT}`)
